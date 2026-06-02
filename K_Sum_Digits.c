3f643bd98f32f2af13f41dd75deaaf2de9c61af1
shamitularefin <shamit.3106@gmail.com>
2026-06-02 23:01:07 +0600

    First commit

---
+++ K_Sum_Digits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+
+    scanf("%d", &n);
+
+    char a[n+1];
+
+    scanf("%s", a);
+
+    int sum = 0;
+
+    for(int i=0; i<n; i++) {
+
+        sum = sum + a[i] - '0';
+
+    }
+
+    printf("%d", sum);
+
+}
